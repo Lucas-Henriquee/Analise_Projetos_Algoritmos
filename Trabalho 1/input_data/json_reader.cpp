36c92d5d9f393fb5bd0b7e12b91f862e92297096
Lucas-Henriquee <lucashn003@hotmail.com>
2025-06-19 19:55:33 -0300

    arrumando repositorio

---
+++ Trabalho 1/input_data/json_reader.cpp
@@ -0,0 +1,45 @@
+#include "../include/defines.hpp"
+
+void read_json_file(const string &filename, deque<size_t> &fibonacci_values, map<double, deque<double>> &polynomial_values)
+{
+    // Open the JSON file
+    ifstream file(filename);
+    // Check if the file is open
+    if (!file.is_open())
+    {
+        cerr << "Error opening file: " << endl;
+        return;
+    }
+    // Parse the JSON file
+    json data = json::parse(file);
+
+    // Close the fileq
+    file.close();
+
+    // Extract the Fibonacci and polynomial data
+    if (data["fibonacci"].empty())
+        cerr << "No Fibonacci data found in the JSON file." << endl;
+
+    else
+    {
+        for (const auto &item : data["fibonacci"])
+            fibonacci_values.push_back(item.get<size_t>());
+    }
+
+    if (data["polynomial"].empty())
+        cerr << "No Polynomial data found in the JSON file." << endl;
+
+    else
+    {
+        for (const auto &item : data["polynomial"])
+        {
+            double x = item["x"].get<double>();
+            deque<double> values;
+
+            for (const auto &term : item["terms"])
+                values.push_back(term.get<double>());
+
+            polynomial_values[x] = values;
+        }
+    }
+}
